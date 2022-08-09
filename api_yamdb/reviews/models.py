19f27e4a81934dcc82b137a6a74c396dc6177bd8
Алтынов Илья <ilusha-altynov@bk.ru>
2022-08-09 13:56:30 +0300

    create models genre,title,category

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    """Жанры произведений."""
+    name = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = 'Жанр'
+        ordering = ('id',)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    """Категории произведение."""
+    name = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        ordering = ('id',)
+
+
+class Title(models.Model):
+    """Произведения."""
+    name = models.CharField(max_length=150)
+    year = models.PositiveIntegerField()
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        verbose_name='Категория',
+        blank=True,
+        null=True,
+        help_text='Выберите категорию'
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        verbose_name='Жанр',
+        help_text='Выберите жанр'
+    )
+
+    class Meta:
+        verbose_name = 'Произведение'
+        ordering = ('id',)
+
+    def __str__(self):
+        return self.name
